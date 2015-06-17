7781cf8e492be5383bba67ed39455e99fa77f430
Jarkko Hautakorpi <jarkko.hautakorpi@gmail.com>
2015-06-17 23:25:27 +0300

    Show the usage of controllerAs View Syntax

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -5,10 +5,21 @@ angular.module('myApp.view2', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
     templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
+    controller: 'View2Ctrl as vm'
   });
 }])
 
 .controller('View2Ctrl', [function() {
-
+        var vm = this;
+        vm.activeVehicle = '';
+        vm.vehicles = {
+            0: {
+                'make': 'Mercedes',
+                'model': 'S 400 Hybrid'
+            },
+            1: {
+                'make': 'Mazda',
+                'model': '6 Sport Wagon'
+            }
+        };
 }]);
