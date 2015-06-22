d8cf0c66b863bb13468e3244673040a4297adc88
Jarkko Hautakorpi <jarkko.hautakorpi@gmail.com>
2015-06-23 00:03:23 +0300

    Sanalista haku

--- app/app.js
+++ app/app.js
@@ -1,13 +1,21 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-        'ngRoute',
-        'ui.bootstrap',
-        'myApp.view1',
-        'myApp.view2',
-        'myApp.version'
-    ]).
-    config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.otherwise({redirectTo: '/view1'});
-    }]);
+var myApp = angular.module('myApp', [
+    'ngRoute',
+    'ui.bootstrap',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+]);
+myApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+myApp.factory("Sanasto", function Sanasto() {
+    var vm = this;
+    return {
+        getLexicon: function () {
+            return window.sanalista["kotus-sanalista"].st;
+        }
+    };
+});
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,27 +2,15 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl as vm'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl as vm'
+        });
+    }])
 
-.controller('View1Ctrl', [function() {
+    .controller('View1Ctrl', function (Sanasto) {
         var vm = this;
-        vm.activePerson = {};
-        vm.persons = {
-            0: {
-                'firstname': 'John',
-                'lastname': 'Doe'
-            },
-            1: {
-                'firstname': 'Mary',
-                'lastname': 'Jane'
-            }
-        };
-        vm.setActivePerson = function (person) {
-            vm.activePerson = person;
-        }
-}]);
+        vm.hakusana = '';
+        vm.sanasto = Sanasto.getLexicon();
+    });
