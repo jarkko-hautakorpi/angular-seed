43f4171f7de6b83a3555704f07782fed714e6d32
Jarkko Hautakorpi <jarkko.hautakorpi@gmail.com>
2016-09-13 23:55:33 +0300

    Chuck Norris API to show a joke.

--- app/app.js
+++ app/app.js
@@ -8,7 +8,24 @@ angular.module('myApp', [
   'ui.bootstrap',
   'myApp.view1',
   'myApp.view2'
-]).
-config(['$routeProvider', function($routeProvider) {
+])
+
+.config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+}])
+
+.service('ChuckNorrisService', function($http){
+    var errorCallback = function (data, status, headers, config) {
+        console.log('ERROR'+data.error.message);
+    };
+    
+    var successCallback = function (data, status, headers, config) {
+        return data;
+    };
+    
+    return {
+        apiGetNorrisJoke: function () {
+            return $http.get('http://api.icndb.com/jokes/random?firstName=Chuck&amp;lastName=Norris').error(errorCallback);
+        }
+      }
+});
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,28 +1,18 @@
 'use strict';
 
 angular.module('myApp.view1', ['ngRoute'])
+  .config(['$routeProvider', function ($routeProvider) {
+      $routeProvider.when('/view1', {
+        templateUrl: 'view1/view1.html',
+        controller: 'View1Ctrl as vm'
+      });
+    }])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl as vm'
+  .controller('View1Ctrl', function (ChuckNorrisService) {
+    var vm = this;
+    vm.joke = {};
+    ChuckNorrisService.apiGetNorrisJoke().then(function(response) {
+      console.debug(response);
+      vm.joke = response.data.value;
+    });
   });
-}])
-
-.controller('View1Ctrl', [function() {
-        var vm = this;
-        vm.activePerson = {};
-        vm.persons = {
-            0: {
-                'firstname': 'John',
-                'lastname': 'Doe'
-            },
-            1: {
-                'firstname': 'Mary',
-                'lastname': 'Jane'
-            }
-        };
-        vm.setActivePerson = function (person) {
-            vm.activePerson = person;
-        }
-}]);
